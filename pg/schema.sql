39b642ef1d6e6ebfe5aaa04887cbcb2ef2e9947a
Hugo Wetterberg <hugo@wetterberg.nu>
2024-02-01 10:05:04 +0100

    fix job lock schema

--- pg/schema.sql
+++ pg/schema.sql
@@ -1,7 +1,6 @@
-CREATE TABLE public.job_lock (
-    name text NOT NULL,
+CREATE TABLE job_lock (
+    name text NOT NULL PRIMARY KEY,
     holder text NOT NULL,
     touched timestamp with time zone NOT NULL,
     iteration bigint NOT NULL
 );
-
