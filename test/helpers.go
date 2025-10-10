ac087d1bddf19cd94c440b0333dd3af33a180698
Hugo Wetterberg <hugo@wetterberg.nu>
2025-10-10 08:00:47 +0200

    improve EqualDiff with generics, add generic golden helper

--- test/helpers.go
+++ test/helpers.go
@@ -1,7 +1,9 @@
 package test
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -65,13 +67,86 @@ func Equal[T comparable](t TestingT, want T, got T, format string, a ...any) {
 }
 
 // EqualDiff runs a cmp.Diff to do a deep equal check with readable diff output.
-func EqualDiff(t TestingT,
-	want any, got any,
+func EqualDiff[T any](t TestingT,
+	want T, got T,
 	format string, a ...any,
 ) {
 	t.Helper()
 
 	diff := cmp.Diff(want, got)
+	if diff != "" {
+		msg := fmt.Sprintf(format, a...)
+		t.Fatalf("%s: mismatch (-want +got):\n%s", msg, diff)
+	}
+
+	if testing.Verbose() {
+		t.Logf("success: "+format, a...)
+	}
+}
+
+// TestAgainstGolden compares a result against the contents of the file at the
+// goldenPath. Run with regenerate set to true to create or update the file.
+func TestAgainstGolden[T any](
+	t *testing.T,
+	regenerate bool,
+	got T,
+	goldenPath string,
+	helpers ...GoldenHelper,
+) {
+	t.Helper()
+
+	if regenerate {
+		data, err := json.Marshal(got)
+		Must(t, err, "marshal result")
+
+		var obj map[string]any
+
+		err = json.Unmarshal(data, &obj)
+		Must(t, err, "unmarshal for transform")
+
+		for i := range helpers {
+			err := helpers[i].JSONTransform(obj)
+			Must(t, err, "transform for storage")
+		}
+
+		data, err = json.MarshalIndent(obj, "", "  ")
+		Must(t, err, "marshal for storage in %q", goldenPath)
+
+		// End all files with a newline
+		data = append(data, '\n')
+
+		err = os.WriteFile(goldenPath, data, 0o600)
+		Must(t, err, "write golden file %q", goldenPath)
+	}
+
+	wantData, err := os.ReadFile(goldenPath)
+	Must(t, err, "read from golden file %q", goldenPath)
+
+	var wantValue T
+
+	err = json.Unmarshal(wantData, &wantValue)
+	Must(t, err, "unmarshal data from golden file %q", goldenPath)
+
+	var cmpOpts cmp.Options
+
+	for _, h := range helpers {
+		cmpOpts = append(cmpOpts, h.CmpOpts()...)
+	}
+
+	EqualDiffWithOptions(t, wantValue, got, cmpOpts,
+		"must match golden file %q", goldenPath)
+}
+
+// EqualMessage runs a cmp.Diff with protobuf-specific options.
+func EqualDiffWithOptions[T any](
+	t TestingT,
+	want T, got T,
+	opts cmp.Options,
+	format string, a ...any,
+) {
+	t.Helper()
+
+	diff := cmp.Diff(want, got, opts...)
 	if diff != "" {
 		msg := fmt.Sprintf(format, a...)
 		t.Fatalf("%s: mismatch (-want +got):\n%s", msg, diff)
